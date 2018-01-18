076d8fd10d1a0e260478b48e4c31fadfbd8ada6c
asapferg <chrobles@microsoft.com>
2018-01-18 15:23:19 -0500

    replace material dropdown with picker dropdown

--- Screens/NewRace.js
+++ Screens/NewRace.js
@@ -7,7 +7,7 @@ import {
   TextInput,
   View
 } from 'react-native'
-import { Dropdown } from 'react-native-material-dropdown'
+import { Picker } from 'react-native-picker-dropdown'
 
 class NewRace extends Component {
   constructor(props) {
@@ -17,28 +17,29 @@ class NewRace extends Component {
 
     this.state = {
       codex: 'R' + d.getHours() + d.getDay() + d.getMonth() + d.getFullYear(),
-      phase: 'Heat 1',
-      gender: 'TestGender',
-      temperature: 'TestTemp',
-      precipitation: 'TestPrecip',
+      phase: 'Heat1',
+      gender: 'F',
+      temperature: '0',
+      precipitation: 'None',
       course: 'DEBUG-CLIENTDEFAULT',
-      racers: Array.apply(null, Array(4)),
+      racers: [],
+      racercount: '4',
       position: 'Start Gate',
       colors: [
-        { value: 'Red',    },
-        { value: 'Green',  },
-        { value: 'Blue',   },
-        { value: 'Yellow', },
+        'Red',
+        'Green',
+        'Blue',
+        'Yellow',
       ]
     }
 
     this.colors = [
-      { value: 'Red',    },
-      { value: 'Green',  },
-      { value: 'Blue',   },
-      { value: 'Yellow', },
-      { value: 'Black',  },
-      { value: 'White',  },
+      'Red',
+      'Green',
+      'Blue',
+      'Yellow',
+      'Black',
+      'White',
     ]
   }
 
@@ -47,7 +48,7 @@ class NewRace extends Component {
   }
 
   componentDidMount() {
-    this.updateRacerCount(4)
+    this.updateRacerCount('4')
   }
 
   updateRacerCount(text) {
@@ -56,7 +57,7 @@ class NewRace extends Component {
 
     for (i in racers) {
       racers[i] = {
-         bibColor: this.colors[i].value,
+         bibColor: this.colors[i],
         startLane: -1,
           holePos: -1,
          splitPos: -1,
@@ -65,10 +66,11 @@ class NewRace extends Component {
     }
 
     this.setState({racers: racers})
+    this.setState({racercount: text})
     this.setState({colors: this.colors.slice(0, count)})
   }
 
-  _handleBtnPress() {
+  onButtonPress() {
     const { navigate } = this.props.navigation
     state = Object.values(this.state)
     if (state.indexOf('') > -1) Alert.alert('Error: enter a value for all fields.')
@@ -76,106 +78,113 @@ class NewRace extends Component {
   }
 
   render() {
-    const genders = [
-      { value: 'Female', },
-      { value: 'Male',   },
-    ]
-    const precipitation = [
-      { value: 'Rain', },
-      { value: 'Snow', }, 
-      { value: 'None', },
-    ]
-    const numRacers = [
-      { value: 4, },
-      { value: 6, },
-    ]
-    const positions = [
-      { value: 'Start Gate', },
-      { value: 'Split',      },
-      { value: 'Hole',       },
-      { value: 'Finish',     },
-    ]
-    const phaseOpt = [
-      { value: 'Heat 1', },
-      { value: 'Heat 2', },
-      { value: 'Heat 3', },
-      { value: 'Heat 4', },
-      { value: 'Heat 5', },
-      { value: 'Heat 6', },
-      { value: 'Quarterfinal 1', },
-      { value: 'Quarterfinal 2', },
-      { value: 'Quarterfinal 3', },
-      { value: 'Quarterfinal 4', },
-      { value: 'Semifinal 1', },
-      { value: 'Semifinal 2', },
-      { value: 'Final', },
-    ]
-
     return (
       <View style={styles.container}>
+      {/* Race Codex */}
         <View style={styles.row}>
+          <Text style={styles.text}>Race Codex</Text>
           <TextInput
             style={styles.textInput}
             value={this.state.codex}
             placeholder={'Race Codex'}
-            onChangeText={(text) => this.setState({ codex: text })}
+            onChangeText={(text)=>this.setState({codex: text})}
           />
         </View>
+      {/* Race Phase */}
         <View style={styles.row}>
-          <Dropdown
-            label='Race Phase'
-            data={phaseOpt}
-            baseColor={'rgba(0, 0, 0, .5)'}
-            containerStyle={styles.input}
-            onChangeText={(text) => this.setState({ phase: text})}
-          />
+          <Text style={styles.text}>Race Phase</Text>
+          <View style={styles.pickerView}>
+            <Picker
+              selectedValue={this.state.phase}
+              onValueChange={(v,i)=>this.setState({phase: v})}
+              mode='dialog'
+              textStyle={styles.pickerText}
+            >
+              <Picker.Item label='Heat 1'         value='Heat1'    />
+              <Picker.Item label='Heat 2'         value='Heat2'    />
+              <Picker.Item label='Heat 3'         value='Heat3'    />
+              <Picker.Item label='Heat 4'         value='Heat4'    />
+              <Picker.Item label='Heat 5'         value='Heat5'    />
+              <Picker.Item label='Heat 6'         value='Heat6'    />
+              <Picker.Item label='Quarterfinal 1' value='Quarter1' />
+              <Picker.Item label='Quarterfinal 2' value='Quarter2' />
+              <Picker.Item label='Quarterfinal 3' value='Quarter3' />
+              <Picker.Item label='Quarterfinal 4' value='Quarter4' />
+              <Picker.Item label='Semifinal 1'    value='Semi1'    />
+              <Picker.Item label='Semifinal 2'    value='Semi2'    />
+              <Picker.Item label='Final'          value='Final'    />
+            </Picker>
+          </View>
         </View>
+      {/* Gender */}
         <View style={styles.row}>
-          <Dropdown
-            label='Gender'
-            data={genders}
-            baseColor={'rgba(0, 0, 0, .5)'}
-            containerStyle={styles.input}
-            onChangeText={(text) => this.setState({gender: text})}
-          />
+          <Text style={styles.text}>Gender</Text>
+          <View style={styles.pickerView}>
+            <Picker
+              selectedValue={this.state.gender}
+              onValueChange={(v,i)=>this.setState({gender: v})}
+              mode='dialog'
+              textStyle={styles.pickerText}
+            >
+              <Picker.Item label='Female' value='F' />
+              <Picker.Item label='Male'   value='M' />
+            </Picker>
+          </View>
         </View>
+      {/* Number of Racers */}
         <View style={styles.row}>
-          <Dropdown
-            label='Number of Racers'
-            data={numRacers}
-            baseColor={'rgba(0, 0, 0, .5)'}
-            containerStyle={styles.input}
-            onChangeText={(text) => this.updateRacerCount(text)}
-          />
+          <Text style={styles.text}>Racer Count</Text>
+          <View style={styles.pickerView}>
+            <Picker
+              selectedValue={this.state.racercount}
+              onValueChange={(v,i)=>this.updateRacerCount(v)}
+              mode='dialog'
+              textStyle={styles.pickerText}
+            >
+              <Picker.Item label='4' value='4' />
+              <Picker.Item label='6' value='6' />
+            </Picker>
+          </View>
         </View>
+      {/* Precipitation */}
         <View style={styles.row}>
-          <Dropdown
-            label='Precipitation'
-            data={precipitation}
-            baseColor={'rgba(0, 0, 0, .5)'}
-            containerStyle={styles.input}
-            onChangeText={(text) => this.setState({precipitation: text})}
-          />
+          <Text style={styles.text}>Precipitation</Text>
+          <View style={styles.pickerView}>
+            <Picker
+              selectedValue={this.state.precipitation}
+              onValueChange={(v,i)=>this.setState({precipitation: v})}
+              mode='dialog'
+              textStyle={styles.pickerText}
+            >
+              <Picker.Item label='Rain' value='Rain' />
+              <Picker.Item label='Snow' value='Snow' />
+              <Picker.Item label='None' value='None' />
+            </Picker>
+          </View>
         </View>
+      {/* Temperature */}
         <View style={styles.row}>
+          <Text style={styles.text}>Temperature</Text>
           <TextInput
             style={styles.textInput}
             value={this.state.temperature}
-            placeholder={'Temperature'}
-            onChangeText={(text) => this.setState({temperature: text})}
+            keyboardType='numeric'
+            onChangeText={(text)=>this.setState({temperature: text})}
           />
         </View>
+      {/* Course Name */}
         <View style={styles.row}>
+          <Text style={styles.text}>Course Name</Text>
           <TextInput
             style={styles.textInput}
             value={this.state.course}
             placeholder={'Course name'}
-            onChangeText={(text) => this.setState({course: text})}
+            onChangeText={(text)=>this.setState({course: text})}
           />
         </View>
-
+      {/* Next Page */}
         <Button
-          onPress={() => this._handleBtnPress()}
+          onPress={() => this.onButtonPress()}
           style={styles.button}
           title='Next >'
         />
@@ -192,7 +201,6 @@ const styles = StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center',
     alignItems: 'center',
     padding: 15,
   },
@@ -202,14 +210,19 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     padding: 5,
   },
-  input: {
-    flex: 1,
-    paddingBottom: 10,
+  pickerView: {
+    flex: 2,
+  },
+  pickerText: {
+    color: 'black',
+    padding: 10,
+    fontSize: 16,
+    textAlign: 'right',
   },
   textInput: {
-    flex: 1,
-    paddingBottom: 7,
+    flex: 2,
     fontSize: 16,
+    textAlign: 'right',
     borderBottomColor: 'rgba(0, 0, 0, .5)',
     borderBottomWidth: .5,
   },
---
+++ idea.js
@@ -0,0 +1,7 @@
+colors = ['r', 'g', 'b', 'y']
+holepos = [-1,-1,-1,-1]
+splitpos=[-1,-1,-1,-1]
+
+// assigning a color
+splitpos[lane] = colors.indexof(color)
+
